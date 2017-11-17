7eb2153caa7f1b8f61b1f999b3db5518bfe0e354
MilosVulikic <milosvulikic@gmail.com>
2017-11-17 16:43:33 +0100

    Factoring Project Adding Scheduler.sc service file Adding Library.cs file Adding Service Installer

---
+++ MyTestWindowsService/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyTestWindowsService
+{
+    public class Library
+    {
+        public static void WriteErrorLog(Exception exception)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString().Trim() + ";" + exception.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+            }
+
+
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString().Trim() + ";" + Message);                
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+            }
+
+
+        }
+
+
+        public static int ProcessNumber()
+        {
+            Random random = new Random();
+            return random.Next(10000, 20000);
+        }
+
+    }
+}
---
+++ MyTestWindowsService/Scheduler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;    // read in details about this class
+
+namespace MyTestWindowsService
+{
+    public partial class Scheduler : ServiceBase
+    {
+        System.Timers.Timer timeDelay1;
+        System.Timers.Timer timeDelay2;
+       
+
+        public Scheduler()
+        {
+            Random rnd = new Random();
+            InitializeComponent();         
+            timeDelay1 = new System.Timers.Timer();
+            timeDelay2 = new System.Timers.Timer();
+            timeDelay1.Interval = rnd.Next(5000,10000);    // 10000 = 10s
+            timeDelay1.Elapsed += new System.Timers.ElapsedEventHandler(this.ProcessStart);
+            timeDelay2.Interval = rnd.Next(7500,15000);
+            timeDelay2.Elapsed += new System.Timers.ElapsedEventHandler(this.ProcessFinish);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timeDelay1.Enabled = true;
+            timeDelay2.Enabled = true;
+            Library.WriteErrorLog("Test window service started");
+        }
+
+        private void ProcessStart(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog(" Process x" + Library.ProcessNumber() + " started");    
+        }
+        private void ProcessFinish(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog(" Process x" + Library.ProcessNumber() + " finished");
+        }
+
+        protected override void OnStop()
+        {
+            timeDelay1.Enabled = true;
+            timeDelay2.Enabled = true;
+              Library.WriteErrorLog("Test window service stopped");
+        }
+    }
+}
